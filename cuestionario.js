4ee135b18f6bca8fc06cdc0ac81b35799fba2195
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-04 03:15:30 +0100

    Update cuestionario.js

--- cuestionario.js
+++ cuestionario.js
@@ -4,11 +4,23 @@ const cameraQuiz = new THREE.PerspectiveCamera(75, window.innerWidth / window.in
 
 // Crear un cubo diferente para la escena del cuestionario
 const geometryQuiz = new THREE.BoxGeometry(); 
-const materialQuiz = new THREE.MeshPhongMaterial({
-  color: 0xFF0000, specular: 0xffffff,
-  shininess: 150,   // red (can also use a CSS color string here)
-  flatShading: true,
-}); // Color inicial: naranja
+const materialQuiz = new THREE.MeshStandardMaterial( {
+
+    color: 0xffffff,
+
+    roughness: roughness,
+    metalness: metalness,
+
+    roughnessMap: roughnessMap,
+    metalnessMap: metalnessMap,
+
+    envMap: envMap, // important -- especially for metals!
+    envMapIntensity: envMapIntensity
+
+} );
+  
+     // red (can also use a CSS color 
+// Color inicial: naranja
 const cubeQuiz = new THREE.Mesh(geometryQuiz, materialQuiz);
 sceneQuiz.add(cubeQuiz);
 
